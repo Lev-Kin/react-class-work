ef925ed5ce056b744cf2cb16213018d187586f21
Leva <parimalleva@gmail.com>
2023-02-25 21:02:48 +0300

    theory privateRoute

--- online-shop/src/components/privateRoute/index.js
+++ online-shop/src/components/privateRoute/index.js
@@ -1,13 +1,17 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
-import { selectIsAuth } from "../../redux/slices/userAuth";
+import React from 'react';
+import {Navigate, Outlet} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectIsAuth } from '../../redux/slices/userAuth';
 
-export const PrivateRoute = ({ children }) => {
-  const isAuth = useSelector(selectIsAuth);
 
-  if(!isAuth){
-    return <Navigate to="/" />
-  }
-  return (<Outlet/>);
+export const PrivateRoute = ({children}) => {
+    const isAuth = useSelector(selectIsAuth);
+
+    if (!isAuth) {
+        return <Navigate to='/'/>
+    }
+
+    return (
+       <Outlet/>
+    )
 };
